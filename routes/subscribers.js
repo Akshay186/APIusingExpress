aabb2aa9e29e3f7914c5b780c4d0c59fe6bf7324
Akshay <ak60702@gmail.com>
2022-05-27 23:41:58 +0530

    Sample api request

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+//geting all
+router.get('/', (req,res)=>{
+    res.send('Hello world!')
+})
+//geting one
+router.get('/', (req,res)=>{
+    
+})
+//creating one
+router.post('/:id', (req,res)=>{
+    
+})
+//updating one
+router.patch('/:id', (req,res)=>{
+    
+})
+//deleting one
+router.delete('/:id', (req,res)=>{
+    
+})
+
 module.exports=router
