cc4b6b235ae19c3fd35723bb684f310b5f97eae4
Akshay <ak60702@gmail.com>
2022-05-27 23:18:54 +0530

    added router file

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ const mongoose = require('mongoose')
 
 const db_url = process.env.DB_URL
 //console.log(db_url)
-
 mongoose.connect(db_url, {useNewUrlParser : true} )
 const db = mongoose.connection
 db.on('error', (error) => console.error('There has been a database connection error', error))
 db.once('open', ()=> console.log('Connected to database'))
+
+app.use(express.json())
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
 
 app.listen(3000, () => console.log('Server started'))
